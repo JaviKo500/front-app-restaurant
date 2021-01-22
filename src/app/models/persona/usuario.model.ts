cc277e4f086ec17f0a124c173283d38a0d5195ea
Javier <javikogutierrez64@gmail.com>
2021-01-21 23:46:24 -0500

    arreglo bug en css side-bar

---
+++ src/app/models/persona/usuario.model.ts
@@ -0,0 +1,14 @@
+import { Role } from './role.model';
+import { Sexo } from './sexo.model';
+export class Usuario {
+    id: number;
+    cedula: string;
+    nombre: string;
+    email: string;
+    // sexo: Sexo;
+    telefono: string;
+    username: string;
+    // password: string;
+    // estado: boolean;
+    // roles: Role [] = [];
+}
--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../models/persona/usuario.model';
 
 @Component({
   selector: 'app-usuarios',
@@ -6,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
-
+  listaUsuarios: Usuario [] = [
+    {
+      id:1,
+      cedula: '0105566046',
+      email: 'javikosasa@gmail.com',
+      nombre: 'Javier Gutierrez',
+      telefono: '0987535645',
+      username: 'JaViKo'
+    },
+    {
+      id:2,
+      cedula: '010012222',
+      email: 'javikqqqqq@gmail.com',
+      nombre: 'Javiesssr Gutierrez',
+      telefono: '0987535645',
+      username: 'JaViKosss'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
