de8e336655544ea1422ec30b3eb828aa38f3529f
marcatoma <marcatoma99@gmail.com>
2021-02-22 15:27:43 -0500

    none

--- src/app/services/combo/combo.service.ts
+++ src/app/services/combo/combo.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BASE_URL } from 'src/environments/configurations';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ComboService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  private url: string = BASE_URL;
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 }
