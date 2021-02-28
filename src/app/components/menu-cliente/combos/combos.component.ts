a6de2e270df5f1e2624df08772197305be19619e
marcatoma <marcatoma99@gmail.com>
2021-02-27 21:18:26 -0500

    error

--- src/app/components/menu-cliente/combos/combos.component.ts
+++ src/app/components/menu-cliente/combos/combos.component.ts
@@ -10,34 +10,31 @@ import { BASE_URL } from '../../../../environments/configurations';
 @Component({
   selector: 'app-combos',
   templateUrl: './combos.component.html',
-  styleUrls: ['./combos.component.css']
+  styleUrls: ['./combos.component.css'],
 })
 export class CombosComponent implements OnInit {
   public modalRef: NgbModalRef;
   combo = new Combo();
   api = BASE_URL;
-  constructor( 
+  constructor(
     private modalService: NgbModal,
-    private activatedRouter: ActivatedRoute, 
+    private activatedRouter: ActivatedRoute,
     private comboService: ComboService
-    ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRouter.paramMap
-      .subscribe( (params) => {
-        let id_combo = +params.get('id');        
-        if (id_combo) {
-          this.comboService.obtenerComboId( id_combo )
-            .subscribe( item => {
-              this.combo = item.combo;              
-            } )
-        }
-      });
+    this.activatedRouter.paramMap.subscribe((params) => {
+      let id_combo = +params.get('id');
+      if (id_combo) {
+        this.comboService.obtenerComboId(id_combo).subscribe((item) => {
+          this.combo = item.combo;
+        });
+      }
+    });
   }
   modalDetalleCombo(modalDetalle): void {
     this.modalRef = this.modalService.open(modalDetalle, {
-      centered: true
+      centered: true,
     });
   }
-
 }
