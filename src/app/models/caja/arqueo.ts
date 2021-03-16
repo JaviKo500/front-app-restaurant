709a62d55e2c1074cf34155e4ca8c06956813143
marcatoma <marcatoma99@gmail.com>
2021-03-16 17:44:48 -0500

    creando registro de arqueo

--- src/app/models/caja/arqueo.ts
+++ src/app/models/caja/arqueo.ts
@@ -5,4 +5,12 @@ export class Arqueo {
   id: number;
   caja: Caja;
   usuario: Usuario;
+  montoInicial: number = 0.0;
+  montoFinal: number = 0.0;
+  efectivo: number = 0.0;
+  bancos: number = 0.0;
+  diferencia: number = 0.0;
+  fecha: Date;
+  fechaApertura: Date;
+  fechaCierre: Date;
 }
