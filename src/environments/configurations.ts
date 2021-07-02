08c3dc20fa2b6872481e4833ef18cdf6bace21b6
marcatoma <marcatoma99@gmail.com>
2021-07-02 15:50:11 -0500

    ip

--- src/environments/configurations.ts
+++ src/environments/configurations.ts
@@ -2,13 +2,13 @@ export const API_PROD = 'register/product/img/upload';
 export const API_CATE = 'register/category/image/upload';
 
 // cris
-// export const BASE_URL = 'http://192.168.10.50:8080/';
-// export const API_QR = 'http://192.168.10.50:4200/#/';
+export const BASE_URL = 'http://192.168.10.50:8080/';
+export const API_QR = 'http://192.168.10.50:4200/#/';
 
 // vini
 // export const BASE_URL = 'http://192.168.100.17:8080/';
 // export const API_QR = 'http://192.168.10.50:4200/#/';
 
-// javi 
-export const BASE_URL = 'http://192.168.1.10:8080/';
-export const API_QR = 'http://192.168.1.10:4200/#/';
+// javi
+//export const BASE_URL = 'http://192.168.1.10:8080/';
+//export const API_QR = 'http://192.168.1.10:4200/#/';
